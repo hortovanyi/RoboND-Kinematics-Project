0df498c5e31f9f9338bc9f869d0198dbccaae0cb
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2017-10-27 16:28:06 +1000

    First working pass

--- FK_debug.py
+++ FK_debug.py
@@ -2,15 +2,27 @@
 
 import numpy as np
 from numpy import array
-from sympy import symbols, cos, sin, pi, simplify, sqrt, atan2
+from sympy import symbols, cos, sin, pi, simplify, sqrt, atan2, lambdify
 from sympy.matrices import Matrix
+from sympy.abc import q, a, d, alpha
+import logging
 
-### Create symbols for joint variables
-q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8') # theta_i
+logger = logging.getLogger('FK_debug')
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
+
+logger.debug("start")
+# Create symbols for joint variables
+q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')  # theta_i
+
+# DH param symbols
 d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
 a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
 alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
+logger.debug("symbols for joint variables defined")
 
 ### KUKA KR210 ###
 # DH Parameters
@@ -19,23 +31,37 @@ alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 # d represents the link offsets
 # Q represents the joint varbles, the thetas
 
-s = {alpha0:    0,  a0: 0,      d1: 0.75,
-     alpha1: -pi/2, a1: 0.35,   d2: 0,      q2: q2-pi/2,
-     alpha2:    0,  a2: 1.25,   d3: 0,
-     alpha3: -pi/2, a3: 0,      d4: 1.50,
-     alpha4:  pi/2, a4: 0,      d5: 0,
-     alpha5: -pi/2, a5: 0,      d6: 0,
-     alpha6:     0, a6: 0,      d7: 0.303,  q7: 0
+s = {alpha0:    0,  a0: 0,      d1: 0.75,   q1: q1,
+     alpha1: -pi/2, a1: 0.35,   d2: 0,      q2: -pi/2. + q2,
+     alpha2:    0,  a2: 1.25,   d3: 0,      q3: q3,
+     alpha3: -pi/2, a3: -0.054, d4: 1.50,   q4: q4,
+     alpha4:  pi/2, a4: 0,      d5: 0,      q5: q5,
+     alpha5: -pi/2, a5: 0,      d6: 0,      q6: q6,
+     alpha6:     0, a6: 0,      d7: 0.303, q7: 0
      }
+logger.debug("DH Parameters defined")
+
 
 ### Homogenous Transforms
+
+# ht_expr = Matrix([[            cos(q),            -sin(q),            0,              a],
+#                [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+#                [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+#                [                  0,                   0,            0,               1]
+#                ])
+# ht_modules = [{'cos': np.cos, 'sin': np.sin, 'ImmutableDenseMatrix': np.matrix}, 'numpy']
+# ht = lambdify((alpha, a, d, q, cos(q), sin(q), cos(alpha), sin(alpha)), ht_expr, ht_modules)
+# T0_1 = ht(alpha0, a0, d1, q1, cos(q1), sin(q1), cos(alpha0), sin(alpha0))
 # base_link to link1
+
 T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0],
                [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
                [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
                [                  0,                   0,            0,               1]
                ])
+logger.debug("T0_1 defined")
 T0_1 = T0_1.subs(s)
+logger.debug("T0_1 subs")
 
 T1_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1],
                [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
@@ -43,6 +69,7 @@ T1_2 = Matrix([[            cos(q2),            -sin(q2),            0,         
                [                  0,                   0,            0,               1]
                ])
 T1_2 = T1_2.subs(s)
+logger.debug("T1_2 subs")
 
 T2_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2],
                [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
@@ -50,6 +77,7 @@ T2_3 = Matrix([[            cos(q3),            -sin(q3),            0,         
                [                  0,                   0,            0,               1]
                ])
 T2_3 = T2_3.subs(s)
+logger.debug("T2_3 subs")
 
 T3_4 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
                [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
@@ -57,6 +85,7 @@ T3_4 = Matrix([[            cos(q4),            -sin(q4),            0,         
                [                  0,                   0,            0,               1]
                ])
 T3_4 = T3_4.subs(s)
+logger.debug("T3_4 subs")
 
 T4_5 = Matrix([[            cos(q5),            -sin(q5),            0,              a4],
                [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
@@ -64,6 +93,7 @@ T4_5 = Matrix([[            cos(q5),            -sin(q5),            0,         
                [                  0,                   0,            0,               1]
                ])
 T4_5 = T4_5.subs(s)
+logger.debug("T4_5 subs")
 
 T5_6 = Matrix([[            cos(q6),            -sin(q6),            0,              a5],
                [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
@@ -71,6 +101,7 @@ T5_6 = Matrix([[            cos(q6),            -sin(q6),            0,         
                [                  0,                   0,            0,               1]
                ])
 T5_6 = T5_6.subs(s)
+logger.debug("T5_6 subs")
 
 T6_G = Matrix([[            cos(q7),            -sin(q7),            0,              a6],
                [sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
@@ -78,14 +109,24 @@ T6_G = Matrix([[            cos(q7),            -sin(q7),            0,         
                [                  0,                   0,            0,               1]
                ])
 T6_G = T6_G.subs(s)
+logger.debug("T6_G subs")
 
 # Composition of Homogenous Transforms
-T0_2 = simplify(T0_1 * T1_2) # base_link to link_2
-T0_3 = simplify(T0_2 * T2_3) # base_link to link_3
-T0_4 = simplify(T0_3 * T3_4) # base_link to link_4
-T0_5 = simplify(T0_4 * T4_5) # base_link to link_5
-T0_6 = simplify(T0_5 * T5_6) # base_link to link_6
-T0_G = simplify(T0_6 * T6_G) # base_link to gripper
+# T0_2 = simplify(T0_1 * T1_2) # base_link to link_2
+# T0_3 = simplify(T0_2 * T2_3) # base_link to link_3
+# T0_4 = simplify(T0_3 * T3_4) # base_link to link_4
+# T0_5 = simplify(T0_4 * T4_5) # base_link to link_5
+# T0_6 = simplify(T0_5 * T5_6) # base_link to link_6
+# T0_G = simplify(T0_6 * T6_G) # base_link to gripper
+# logger.debug("simplifies done")
+
+T0_2 = T0_1 * T1_2 # base_link to link_2
+T0_3 = T0_2 * T2_3 # base_link to link_3
+T0_4 = T0_3 * T3_4 # base_link to link_4
+T0_5 = T0_4 * T4_5 # base_link to link_5
+T0_6 = T0_5 * T5_6 # base_link to link_6
+T0_G = T0_6 * T6_G # base_link to gripper
+logger.debug("matrix multiplications done")
 
 # Correction Needd to Account of Orientation Difference BEtween Deinition of
 # Gripper_link in URDF versus DH Convention
@@ -99,19 +140,35 @@ R_y = Matrix([[ cos(-np.pi/2),  0,  sin(-np.pi/2), 0],
               [-sin(-np.pi/2),  0,  cos(-np.pi/2), 0],
               [             0,  0,              0, 1]
               ])
-R_corr = simplify(R_z * R_y)
+# R_corr = simplify(R_z * R_y)
+R_corr = R_z * R_y
+logger.debug("R_corr ")
 
 #### Numerically evalute transforms (compare this with output of tf_echo!)
-joints={q1:-1.54, q2:0.95, q3: -1.54, q4: 3.65, q5: -1.02, q6:-5.07}
+joints={q1: 3.19, q2:0.75, q3: -2.77, q4: -2.14, q5: -0.33, q6:-0.04}
+# joints={q1:0, q2:0, q3: 0, q4: 0, q5: 0, q6:0}
+
 print("T0_1 = ", T0_1.evalf(subs=joints))
+logger.debug("T0_1")
 print("T0_2 = ", T0_2.evalf(subs=joints))
+logger.debug("T0_2")
 print("T0_3 = ", T0_3.evalf(subs=joints))
+logger.debug("T0_3")
 print("T0_4 = ", T0_4.evalf(subs=joints))
+logger.debug("T0_4")
 print("T0_5 = ", T0_5.evalf(subs=joints))
+logger.debug("T0_5")
 print("T0_6 = ", T0_6.evalf(subs=joints))
+logger.debug("T0_6")
 print("T0_G = ", T0_G.evalf(subs=joints))
+logger.debug("T0_G")
 
 # Total Homogenous Transforms Between Base_link and Gripper_link with
 # Orientation Correction Applied
-T_total = simplify(T0_G * R_corr)
+# T_total = simplify(T0_G * R_corr)
+# logger.debug("T_total simplify")
+T_total = T0_G * R_corr
+logger.debug("T_total matrix multiply")
 print("T_total = ", T_total.evalf(subs=joints))
+
+logger.debug("finish")
--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -24,59 +24,188 @@ def handle_calculate_IK(req):
     if len(req.poses) < 1:
         print "No valid poses received"
         return -1
-    else:
-		
-        ### Your FK code here
-        # Create symbols
-	#
-	#   
+
+    # Your FK code here
+
+    # Create symbols
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')  # theta_i
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
 	# Create Modified DH parameters
-	#
-	#            
-	# Define Modified DH Transformation matrix
-	#
-	#
+    # alpha represents the twist angles
+    # a represents the link lengths
+    # d represents the link offsets
+    # Q represents the joint varbles, the thetas
+    s = {alpha0:    0,  a0: 0,      d1: 0.75,   q1: q1,
+         alpha1: -pi/2, a1: 0.35,   d2: 0,      q2: -pi/2. + q2,
+         alpha2:    0,  a2: 1.25,   d3: 0,      q3: q3,
+         alpha3: -pi/2, a3: -0.054, d4: 1.50,   q4: q4,
+         alpha4:  pi/2, a4: 0,      d5: 0,      q5: q5,
+         alpha5: -pi/2, a5: 0,      d6: 0,      q6: q6,
+         alpha6:     0, a6: 0,      d7: 0.303, q7: 0
+         }
+
 	# Create individual transformation matrices
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-	#
-	#
+    T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0],
+                    [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                    [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                    [                  0,                   0,            0,               1]
+                    ])
+    T0_1 = T0_1.subs(s)
+
+    T1_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1],
+                    [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                    [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                    [                  0,                   0,            0,               1]
+                    ])
+    T1_2 = T1_2.subs(s)
+
+    T2_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2],
+                    [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+                    [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+                    [                  0,                   0,            0,               1]
+                    ])
+    T2_3 = T2_3.subs(s)
+
+    T3_4 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
+                    [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                    [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                    [                  0,                   0,            0,               1]
+                    ])
+    T3_4 = T3_4.subs(s)
+
+    T4_5 = Matrix([[            cos(q5),            -sin(q5),            0,              a4],
+                    [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                    [sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                    [                  0,                   0,            0,               1]
+                    ])
+    T4_5 = T4_5.subs(s)
+
+    T5_6 = Matrix([[            cos(q6),            -sin(q6),            0,              a5],
+                    [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                    [sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                    [                  0,                   0,            0,               1]
+                    ])
+    T5_6 = T5_6.subs(s)
+
+    T6_G = Matrix([[            cos(q7),            -sin(q7),            0,              a6],
+                    [sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+                    [sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+                    [                  0,                   0,            0,               1]
+                    ])
+    T6_G = T6_G.subs(s)
+
+    T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
+
+    # Extract rotation matrices from the transformation matrices
+    # Define RPY rotation matrices
+    # http://planning.cs.uiuc.edu/node102.html
+    r, p, y = symbols('r p y')
+
+    ROT_x = Matrix([[1, 0,0],
+                    [0, cos(r), -sin(r)],
+                    [0, sin(r), cos(r)]
+                    ])  # ROLL
+    ROT_y = Matrix([[cos(p), 0, sin(p)],
+                    [0, 1, 0],
+                    [-sin(p), 0, cos(p)]
+                    ])  # PITCH
+    ROT_z = Matrix([[cos(y), -sin(y), 0],
+                    [sin(y), cos(y), 0],
+                    [0, 0, 1]
+                    ])  # YAW
+
+    # Gripper_link in URDF versus DH Convention
+    # R_z = Matrix([[cos(pi), -sin(pi), 0, 0],
+    #               [sin(pi),  cos(pi), 0, 0],
+    #               [      0,         0, 1, 0],
+    #               [      0,         0, 0, 1]
+    #               ])
+    # R_y = Matrix([[ cos(-pi/2.), 0, sin(-pi/2.), 0],
+    #               [           0, 1,              0, 0],
+    #               [-sin(-pi/2.), 0,  cos(-pi/2.), 0],
+    #               [           0, 0,               0, 1]
+    #               ])
+    # # R_corr = simplify(R_z * R_y)
+    # R_corr = R_z * R_y
+
+    # Initialize service response
+    joint_trajectory_list = []
+    for x in xrange(0, len(req.poses)):
+        # IK code starts here
+        joint_trajectory_point = JointTrajectoryPoint()
+
+        # Extract end-effector position and orientation from request
+        # px,py,pz = end-effector position
+        # roll, pitch, yaw = end-effector orientation
+        px = req.poses[x].position.x
+        py = req.poses[x].position.y
+        pz = req.poses[x].position.z
+
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+            [req.poses[x].orientation.x, req.poses[x].orientation.y,
+                req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+        # Your IK code here
+        # Compensate for rotation discrepancy between DH parameters and Gazebo
+        # Calculate joint angles using Geometric IK method
         ###
+        Rot_Error = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
 
-        # Initialize service response
-        joint_trajectory_list = []
-        for x in xrange(0, len(req.poses)):
-            # IK code starts here
-            joint_trajectory_point = JointTrajectoryPoint()
+        ROT_EE = ROT_z * ROT_y * ROT_x
+        ROT_EE = ROT_EE * Rot_Error
+        ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
 
-	    # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
-            px = req.poses[x].position.x
-            py = req.poses[x].position.y
-            pz = req.poses[x].position.z
+        EE = Matrix([[px],
+                     [py],
+                     [pz]])
+        # Calculate joint angles using Geometric IK method
+        ###
+        # Wrist Center
+        WC = EE - (0.303) * ROT_EE[:,2]
 
-            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-                [req.poses[x].orientation.x, req.poses[x].orientation.y,
-                    req.poses[x].orientation.z, req.poses[x].orientation.w])
-     
-            ### Your IK code here 
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    #
-	    #
-	    # Calculate joint angles using Geometric IK method
-	    #
-	    #
-            ###
-		
-            # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+        # Calculate joint angles using Geometrix IK method
+        # More information can be found in the Inverse Kinematics with Kuka KR210
+        theta1 = atan2(WC[1], WC[0])
 
-        rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
-        return CalculateIKResponse(joint_trajectory_list)
+        # SSS triangle for theta2 and theta3
+        side_a = 1.501
+        side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
+        side_c = 1.25
+
+        side_a_sq = side_a * side_a
+        side_b_sq = side_b * side_b
+        side_c_sq = side_c * side_c
+
+        angle_a = acos((side_b_sq + side_c_sq - side_a_sq)/(2 * side_b * side_c))
+        angle_b = acos((side_a_sq + side_c_sq - side_b_sq)/(2 * side_a * side_c))
+        angle_c = acos((side_a_sq + side_b_sq - side_c_sq)/(2 * side_a * side_b))
+
+        theta2 = pi/2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
+        theta3 = pi/2 - (angle_b + 0.036)  # 0.036 accounts for sag in link4 of -0.054 m
+
+        # R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+        T0_3 = T0_1 * T1_2 * T2_3
+        R0_3 = T0_3[0:3,0:3]
+        R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+
+        R3_6 = R0_3.inv("LU") * ROT_EE
+
+        # Euler angles from rotation matrix
+        # More information can be found in the Euler Angles from a Rotation Matrix section
+        theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+        theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]), R3_6[1,2])
+        theta6 = atan2(-R3_6[1,2], R3_6[1,0])
+
+        # Populate response for the IK request
+        # In the next line replace theta1,theta2...,theta6 by your joint angle variables
+        joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+        joint_trajectory_list.append(joint_trajectory_point)
+
+    rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+    return CalculateIKResponse(joint_trajectory_list)
 
 
 def IK_server():
